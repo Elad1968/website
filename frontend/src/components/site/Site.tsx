6cbca7448f30cf3331ccd62b3d2074068d4e3892
elad1971 <mail@eladhuttner.net>
2025-07-02 20:54:26 +0300

    Added a devider between sections.

--- frontend/src/components/site/Site.tsx
+++ frontend/src/components/site/Site.tsx
@@ -7,6 +7,15 @@ import Projects from "./projects/Projects";
 import Skills from "./skills/Skills";
 
 export default function Site() {
+    const mainElements = [<About />, <Skills />, <Projects />, <Contact />] as const;
+    const colors = [
+        "bg-[var(--rainbow-orange)]",
+        "bg-[var(--rainbow-yellow)]",
+        "bg-[var(--rainbow-green)]",
+        "bg-[var(--rainbow-blue)]",
+        "bg-[var(--rainbow-indigo)]",
+    ] as const;
+    const degrees = ["rotate-[2.5deg]", "-rotate-[2.5deg]"] as const;
     return (
         <div
             className="flex flex-col p-0 m-0 w-screen min-h-screen bg-orange-800 max-[256px]:hidden"
@@ -17,10 +26,18 @@ export default function Site() {
             <Header />
             <main className="flex flex-col grow-1">
                 <Introduction />
-                <About />
-                <Skills />
-                <Projects />
-                <Contact />
+                {mainElements.map((element, index) => {
+                    return (
+                        <div key={index} className={`${colors[index]}`}>
+                            <hr
+                                className={`my-[calc((100vw/2)*tan(2.5deg))] flex-shrink-1 relative border-black border-y-2 border-dashed ${
+                                    degrees[index % 2]
+                                }`}
+                            />
+                            {element}
+                        </div>
+                    );
+                })}
             </main>
             <Footer />
         </div>
