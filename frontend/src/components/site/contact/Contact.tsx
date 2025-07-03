c72e8e5ec3b6420838d254eeebc294628a05434b
elad1971 <mail@eladhuttner.net>
2025-07-03 11:56:27 +0300

    Fixed a typo in the subheader of the contact section.

--- frontend/src/components/site/contact/Contact.tsx
+++ frontend/src/components/site/contact/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
         <section id="contact" className="flex flex-col items-center gap-4 screen">
             <SectionHeader>Contact me</SectionHeader>
             <span className="text-center max-w-128">
-                If you like what you saw, feel free to contact me. The form below send me an email and lets me know you
+                If you like what you saw, feel free to contact me. The form below sends me an email and lets me know you
                 have reached out.
             </span>
             <ContactForm url={contact.url} />
