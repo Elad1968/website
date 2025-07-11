5197ba2cf1942109f8ee87d2fb6c55fa0a9ca28f
elad1971 <mail@eladhuttner.net>
2025-07-11 20:21:49 +0300

    Tweaked the animation duration.

--- frontend/src/components/site/projects/Projects.tsx
+++ frontend/src/components/site/projects/Projects.tsx
@@ -65,7 +65,7 @@ export default function Projects() {
                                         initial={{ opacity: 0, y: 20 }}
                                         animate={{ opacity: 1, y: 0 }}
                                         exit={{ opacity: 0, y: -20 }}
-                                        transition={{ duration: 0.3 }}
+                                        transition={{ duration: 0.25 }}
                                     >
                                         <Project
                                             title={projectName}
