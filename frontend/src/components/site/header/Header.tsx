457b64007fb958e7b076ea8479b60a4d51f81110
elad1971 <mail@eladhuttner.net>
2025-08-29 10:40:40 +0300

    Fixed a bug where the icon int the theme picker didn't show in light mode.

--- frontend/src/components/site/header/Header.tsx
+++ frontend/src/components/site/header/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
             <h1 className="m-0 text-4xl hover:text-green-500">
                 <a href="#">{general.name}</a>
             </h1>
-            <ThemeSwitcher />
+            <ThemeSwitcher className="text-foreground" />
             <nav className="flex flex-row justify-around gap-4 max-lg:hidden">
                 <NavLink href="#about">About</NavLink>
                 <NavLink href="#skills">Skills</NavLink>
