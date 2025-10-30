1666b1eaad60e8104b03e82b835574316bda3b8b
elad1971 <mail@eladhuttner.net>
2025-10-30 09:29:32 +0200

    Shortened even more tailwind classes where possible.

--- frontend/src/components/site/contact/Contact.tsx
+++ frontend/src/components/site/contact/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <section id="contact" className="flex flex-col items-center gap-4 screen">
             <SectionHeader>Contact me</SectionHeader>
-            <span className="text-center max-w-128">
+            <span className="text-center max-w-lg">
                 If you like what you saw, feel free to contact me. The form below sends me an email and lets me know you
                 have reached out.
             </span>
